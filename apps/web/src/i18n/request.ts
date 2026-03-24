d6a10b370cb162bac317f3179a0ee5647369d8f9
Oleksandr Barishpolets <40526480+saniaBarish@users.noreply.github.com>
2026-03-24 11:40:21 +0200

    fix: lint error

--- apps/web/src/i18n/request.ts
+++ apps/web/src/i18n/request.ts
@@ -1,10 +1,11 @@
 import { getRequestConfig } from 'next-intl/server';
 import { routing } from './routing';
+import { ExistedLocale } from './types';
 
 export default getRequestConfig(async (params) => {
   let locale = await params.requestLocale;
 
-  if (!routing.locales.includes(locale as any)) {
+  if (!routing.locales.includes(locale as ExistedLocale)) {
     locale = routing.defaultLocale;
   }
 
